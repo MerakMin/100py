dc14815a6424313a335a49ed7460fad92248662b
MerakMin <ssmin413@gmail.com>
2022-10-15 20:02:41 +0900

    eightysecond commit

---
+++ 82py.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for i in range(0, n):
+    i += 1
+    
+    if i%10==3:
+        print('X', end=' ')
+   
+    elif i%10==6:
+        print('X', end=' ')
+
+    elif i%10==9:
+        print('X', end=' ')
+    
+    else:
+        print(i, end = ' ')
